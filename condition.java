fe4f913bc7f90a03458f3392b5ca9d4227af5bf3
willcurse <136434342+willcurse@users.noreply.github.com>
2023-07-09 19:32:57 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,39 @@
+import java.io.*;
+    import java.util.*;
+    import java.text.*;
+    import java.math.*;
+    import java.util.regex.*;
+
+
+public class condition {
+    public static void main(String[] args) {
+    
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n= sc.nextInt();
+ 
+        
+            if(n%2==1){
+                System.out.println("Weird");
+            }
+        else{
+            if(n>=2&&n<=5){
+                System.out.println("Not Weird");
+            }
+            else if(n>=6&&n<=20){
+                System.out.println("Weird");
+            }
+            if(n%2==0){
+                System.out.println("Not Weird");
+            }
+            
+        }
+
+         
+        
+
+        }
+        
+    }
+    
+
